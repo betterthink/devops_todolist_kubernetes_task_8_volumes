7c01d6265385b48687849f55f40c492b8ee526c0
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-04-11 18:58:34 +0300

    fixing bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 cd .infrastructure
 
-kubectl apply -f namespace.yml
-
-kubectl apply -f configMap.yml
-kubectl apply -f secret.yml
-
-kubectl apply -f clusterIp.yml
-kubectl apply -f nodeport.yml
-
 kubectl apply -f pv.yml
 kubectl apply -f pvc.yml
-
-kubectl apply -f hpa.yml
 
 kubectl apply -f deployment.yml
